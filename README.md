44e0016c3cb83d88554bd24fd6dca8256510021c
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-09-23 14:59:11 +0600

    write basic code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Frontend & Backend are or different server.
 Frontend: React-Vite
 Backend: Django-REST API
+
+### File-Folder details
+1. "backend" is a django-project handling requests and providing apis.
+2. "frontend" the frontend Interface that is a React-Vite Application.
+
--- backend/rootapp/models.py
+++ backend/rootapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+__models__ = [
+    'User',
+    'Contact',
+    'Phone'
+]
+
+
+# class Contact(models.Model):
+#     first_name = models.CharField(max_length=60)
+#     middle_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     nick_name = models.CharField(max_length=20)
