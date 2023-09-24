d04a775c0d5b81dff835281a8883d78f1dc6e67b
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-09-24 14:53:21 +0600

    modify SearchBar

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar() {
   return (
     <form className="flex space-x-2 justify-center items-center py-2">
       <input type="text" placeholder="search contact" className="input-field" />
+      {/* <TextField placeholder="search contact" variant="outlined" size="small" color="info"/> */}
       <Button type="submit" variant="contained">
         <SearchIcon/>
       </Button>
